7aa32f198af01c83879761e883c437eed856f0e1
Maciej Małecki <me@mmalecki.com>
2016-01-22 09:25:26 +0100

    Implement `numbat-netstat`

---
+++ index.js
@@ -0,0 +1,41 @@
+var bole = require('bole')
+var NumbatEmitter = require('numbat-emitter')
+var procfs = require('procfs-stats')
+var linuxTCPStates = require('linux-tcp-states')
+
+var DEFAULT_INTERVAL = 1000
+
+var NetstatProducer = module.exports = function (options) {
+  var emitter = new NumbatEmitter(options)
+
+  setInterval(produce, options.interval || DEFAULT_INTERVAL)
+
+  function produceSocketsByState() {
+    procfs.tcp(function (err, sockets) {
+      if (err) return emitter.metric('netstat.error')
+
+      var states = {}
+      sockets.forEach(function (socket) {
+        var state = linuxTCPStates[parseInt(socket.st, 16)]
+        if (!states[state]) states[state] = 1
+        else ++states[state]
+      })
+
+      Object.keys(states).forEach(function (state) {
+        console.log(states[state], state)
+        emitter.metric({
+          name: 'sockets.' + state,
+          value: states[state]
+        })
+      })
+    })
+  }
+
+  function produce() {
+    // What we're looking for is:
+    // * sockets by state
+    // * bytes in, bytes out (TODO)
+    // * retransmissions! (TODO)
+    produceSocketsByState()
+  }
+}
