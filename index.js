1368751a83be08507dd841edf7a102eac7b2c25a
Maciej Małecki <me@mmalecki.com>
2016-07-25 11:45:53 -0700

    Track TCP retransmissions by target host

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var DEFAULT_INTERVAL = 1000
 
 var NetstatProducer = module.exports = function (options) {
   var emitter = new NumbatEmitter(options)
+  var previousRetransmissions = new Map()
+
   emitter.metric('sockets.monitor.start')
 
   setInterval(produce, options.interval || DEFAULT_INTERVAL)
@@ -18,10 +20,21 @@ var NetstatProducer = module.exports = function (options) {
       if (err) return emitter.metric('netstat.error')
 
       var states = {}
+      var retransmissions = new Map()
+
       sockets.forEach(function (socket) {
+        var host
+        var countForHost
         var state = linuxTCPStates[parseInt(socket.st, 16)]
+
         if (!states[state]) states[state] = 1
         else ++states[state]
+
+        if (socket.retrnsmt > 0) {
+          host = socket.rem_address.split(':')[0]
+          countForHost = retransmissions.get(host) || 0
+          retransmissions.set(host, countForHost + parseInt(socket.retrnsmt, 10))
+        }
       })
 
       Object.keys(states).forEach(function (state) {
@@ -30,6 +43,15 @@ var NetstatProducer = module.exports = function (options) {
           value: states[state]
         })
       })
+
+      for (var value of retransmissions) {
+        emitter.metric({
+          name: 'sockets.retransmissions.' + value[0],
+          value: value[1] - (previousRetransmissions.get(value[0]) || 0)
+        })
+      }
+
+      previousRetransmissions = retransmissions
     })
   }
 
